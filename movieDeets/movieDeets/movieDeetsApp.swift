fc15b8ce7c093d0cbe2d54c017d848a840a89092
Stuart Ziane <68736465+stuartziane@users.noreply.github.com>
2022-12-14 12:48:52 +0000

    created xcode project

---
+++ movieDeets/movieDeets/movieDeetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieDeetsApp.swift
+//  movieDeets
+//
+//  Created by Stuart Ziane on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct movieDeetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
